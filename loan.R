8cbbf3e9438933bef055fd5a90c4b80f1a6933a1
Akshay Nevrekar <Akshay Nevrekar>
2017-04-29 21:38:35 +0530

    changes done

--- loan.R
+++ loan.R
@@ -5,11 +5,23 @@ library(ggplot2)
 train <- read.csv("E:/DataScience/Git/TechGigMachineLearning/train_data.csv", header= T, stringsAsFactors= F)
 test <- read.csv("E:/DataScience/Git/TechGigMachineLearning/test_data.csv",header = T, stringsAsFactors = F)
 
-test$Loan_Status <- "NA"
-allData <- rbind(train,test)
-
-allData$Gender <- as.factor(allData$Gender)
-
-
 str(train)
+summary(train)
+str(test)
+summary(test)
+
+test$Loan_Status <- "NA"      #adding column "Loan_Status" into "test" dataset
+allData <- rbind(train,test)   #Combining test and train dataset
+
+#converting features into factors
+allData$Gender <- as.factor(allData$Gender)
+allData$Married <- as.factor(allData$Married)
+allData$Dependents <- as.factor(allData$Dependents)
+allData$Education <- as.factor(allData$Education)
+allData$Self_Employed <- as.factor(allData$Self_Employed)
+allData$Credit_History <- as.factor(allData$Credit_History)
+allData$Property_Area <- as.factor(allData$Property_Area)
+allData$Loan_Status <- as.factor(allData$Loan_Status)
+allData$Loan_Amount_Term <- as.factor(allData$Loan_Amount_Term)
+
 
